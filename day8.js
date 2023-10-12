d939daa7bc3ecd813f6085ad8a1746930de09f0e
OmkarBhosale07 <omkarbhosale5484@gmail.com>
2023-10-12 09:20:05 +0530

    Day 8 Code

---
+++ day8.js
@@ -0,0 +1,14 @@
+   /**
+ * Returns a string with the pet name
+ * @param {string} petName
+ * @param {string} [placeholder= "{Name}"]
+ * @returns {string} Email String
+ */
+export function updateTemplateLiteral(petName, placeholder = "{NAME}") {
+    const reminderTemplate =
+      "{NAME} is due for another visit. Please call us so we can set up a new appointment. We look forward to seeing you and {NAME} soon.";
+    return reminderTemplate.replace(new RegExp(placeholder, 'g'), petName);
+  }
+// Test your code
+console.log(updateTemplateLiteral("Leo"));
+console.log(updateTemplateLiteral("Stella", "{NAME}"));

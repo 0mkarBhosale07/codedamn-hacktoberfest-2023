0a834a00a9c1922c534f8900658dfeccaba0d573
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2023-10-26 23:16:01 +0500

    Update day7.js
    The issue with the code provided is that it modifies the original array (by using array.push(element)), but it doesn't return the sorted array    // Clone the original array and add the new element
      const newArray = [...array, element];
      // Sort the new array in ascending order
      newArray.sort((a, b) => a - b);
      return newArray;
    }

--- day7.js
+++ day7.js
@@ -1,22 +1,18 @@
-/**
- * The functions returns a sorted array after adding the new element
+
+ * The function returns a sorted array after adding the new element
  * @param {number[]} array
  * @param {number} element
  * @returns {number[]}
  */
-export function addAndSort(array, element) {
-	// return array.push(element).sort()
-
-
-	// return array.concat(element).sort()
-    array.push(element);
-  array.sort((a, b) => a - b); // Sort the array in ascending order
-  return array;
-
-    
+export function addAndSorting(array, element) {
+  // Clone the original array and add the new element
+  const newArray = [...array, element];
+  // Sort the new array in ascending order
+  newArray.sorting((a, b) => a - b);
+  return newArray;
 }
 
-// Sample Test Cases
-console.log(addAndSort([5, 7, 1, 6], 3)); // Expected Output: [1, 3, 5, 6, 7]
-console.log(addAndSort([15, 7, 2, 26], 10)); // Expected Output: [2, 7, 10, 15, 26]
-console.log(addAndSort([25, 27, 21, 16], 19)); // Expected Output: [16, 19, 21, 25, 27]
+console.log(addAndSorting([5, 7, 1, 6], 3)); // Expected Output: [1, 3, 5, 6, 7]
+console.log(addAndSorting([15, 7, 2, 26], 10)); // Expected Output: [2, 7, 10, 15, 26]
+console.log(addAndSorting([25, 27, 21, 16], 19)); // Expected Output: [16, 19, 21, 25, 27]
+
